82b98f50260a27a8c40884cdd11a54d86f451ce3
anyul996 <9965421@gmail.com>
2025-06-16 20:20:59 +0800

    优化和同步meme_emoji，meme-generator-jj表情

---
+++ meme_generator/memes/nizaishuo/__init__.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+from meme_generator.utils import make_jpg_or_gif
+
+img_dir = Path(__file__).parent / "images"
+
+default_text = "02大撒杯，闭嘴！"
+
+
+def nizaishuo(images: list[BuildImage], texts: list[str], args):
+    frame = BuildImage.open(img_dir / "0.png")
+    text = f"{texts[0]}"
+    text2image = Text2Image.from_text(
+        text, 15, fill=(0, 0, 0), stroke_width=3, stroke_fill="black",
+        font_families=["033-SSFangTangTi"]
+    ).wrap(200)
+    if text2image.height > 80:
+        raise TextOverLength(text)
+    text_img = text2image.to_image()
+
+    def make(imgs: list[BuildImage]) -> BuildImage:
+        text_w, _ = text_img.size
+        x = (frame.width - text_w) // 2  # 居中对齐的x坐标
+        frame.paste(text_img, (x, 12), alpha=True)
+        img = imgs[0].convert("RGBA").resize((60, 50), keep_ratio=True, inside=True)
+        return frame.copy().paste(img, (88, 14), alpha=True)
+
+    return make_jpg_or_gif(images, make)
+
+
+add_meme(
+    "nizaishuo",
+    nizaishuo,
+    min_images=1,
+    max_images=1,
+    min_texts=0,
+    max_texts=1,
+    default_texts=[default_text],
+    keywords=["你再说", "你闭嘴"],
+    date_created=datetime(2025, 6, 16),
+    date_modified=datetime(2025, 6, 16),
+)
